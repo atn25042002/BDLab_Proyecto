ce1f142d11f30a75240f99fe2abb0f5c9cde0135
unknown <Aacot@unsa.edu.pe>
2023-07-01 11:47:07 -0500

    Arreglando multiplicidad

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,34 +1,57 @@
-function changeColor(val){
-    document.getElementById("modify"+val).setAttribute("class", "btn btn-primary")
-    document.getElementById("remove"+val).setAttribute("class", "btn btn-primary")
-    document.getElementById("inactivate"+val).setAttribute("class", "btn btn-primary")
-    document.getElementById("reactivate"+val).setAttribute("class", "btn btn-primary")
+function changeColor(){
+    document.getElementById("modify").setAttribute("class", "btn btn-primary")
+    document.getElementById("remove").setAttribute("class", "btn btn-primary")
+    document.getElementById("inactivate").setAttribute("class", "btn btn-primary")
+    document.getElementById("reactivate").setAttribute("class", "btn btn-primary")
 }
 
-function mod(val){
-    changeColor(val)
-    document.getElementById("modify"+val).setAttribute("class", "btn btn-success")
-    document.getElementById("nom"+val).removeAttribute("readonly")
-    document.getElementById("est"+val).setAttribute("value", "A")
+function mod(){
+    changeColor()
+    document.getElementById("modify").setAttribute("class", "btn btn-success")
+    document.getElementById("nomMod").removeAttribute("readonly")
+    document.getElementById("estMod").setAttribute("value", "A")
 }
 
-function del(val){
-    changeColor(val)
-    document.getElementById("remove"+val).setAttribute("class", "btn btn-danger")
-    document.getElementById("nom"+val).setAttribute("readonly", "true")
-    document.getElementById("est"+val).setAttribute("value", "*")
+function del(){
+    changeColor()
+    document.getElementById("remove").setAttribute("class", "btn btn-danger")
+    document.getElementById("nomMod").setAttribute("readonly", "true")
+    document.getElementById("estMod").setAttribute("value", "*")
 }
 
-function inac(val){
-    changeColor(val)
-    document.getElementById("inactivate"+val).setAttribute("class", "btn btn-success")
-    document.getElementById("nom"+val).setAttribute("readonly", "true")
-    document.getElementById("est"+val).setAttribute("value", "I")
+function inac(){
+    changeColor()
+    document.getElementById("inactivate").setAttribute("class", "btn btn-success")
+    document.getElementById("nomMod").setAttribute("readonly", "true")
+    document.getElementById("estMod").setAttribute("value", "I")
 }
 
-function reac(val){
-    changeColor(val)
-    document.getElementById("reactivate"+val).setAttribute("class", "btn btn-success")
-    document.getElementById("nom"+val).setAttribute("readonly", "true")
-    document.getElementById("est"+val).setAttribute("value", "A")
+function reac(){
+    changeColor()
+    document.getElementById("reactivate").setAttribute("class", "btn btn-success")
+    document.getElementById("nomMod").setAttribute("readonly", "true")
+    document.getElementById("estMod").setAttribute("value", "A")
+}
+
+function nuevo(){
+    document.getElementById("titMod").setAttribute("value", "Agregando nuevo producto")
+    document.getElementById("codMod").setAttribute("value", "")
+    document.getElementById("nomMod").setAttribute("value", "")
+    document.getElementById("estMod").setAttribute("value", "")
+
+    document.getElementById("formMod").setAttribute("action", "/section/")
+}
+
+function llenar(val){
+    document.getElementById("titMod").innerHTML= ("Editando " + val)
+    const cod= document.getElementById("cod" + val).innerHTML
+    document.getElementById("codMod").setAttribute("value", cod)
+    const nom= document.getElementById("nom" + val).innerHTML
+    document.getElementById("nomMod").setAttribute("value", nom)
+    const est= document.getElementById("est" + val).innerHTML
+    document.getElementById("estMod").setAttribute("value", est)
+
+    document.getElementById("formMod").setAttribute("action", "/edit/" + cod)
+    ///edit/{{d.SecCod}}
+    //value="{{d.SecEstReg.copy().pop()}}" 
 }
